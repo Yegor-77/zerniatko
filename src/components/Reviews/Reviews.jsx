72a6ed2d4fafa47eca514db942fbb2ca62ed4304
Yegor-77 <egorhmeluk77@gmail.com>
2026-06-22 19:32:59 +0200

    Y-P-3

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,57 @@
 
-export default function Reviews(){
-    return (<h2>Відгуки Клієнтів</h2>
-    )
+export default function Reviews() {
+
+
+  const Reviews = [
+    {
+      com: ". . . . .",
+      text: "Дуже задоволена покупкою! Кавоварка компактна, зручна й стильна. Кава виходить насичена, з кремовою пінкою — майже як у кав'ярні.",
+      name: "Анна Коваленко"
+    },
+  ];
+
+  return (
+
+    <section className="reviews">
+
+
+      <div className="top">
+
+        <h2>Відгуки клієнтів</h2>
+
+        <button>
+          Залишити відгук
+        </button>
+
+      </div>
+
+      <div className="list">
+        {reviews.map((item,index)=>(
+          <div className="card" key={index}>
+            <div className="dots">
+              {item.dots}
+            </div>
+            <p>
+              {item.text}
+            </p>
+         <b>
+              {item.name}
+            </b>
+        </div>
+        ))}
+      </div>
+      <div className="buttons">
+
+        <button>←</button>
+
+        <button>→</button>
+
+      </div>
+
+
+
+    </section>
+
+  );
+
 }
