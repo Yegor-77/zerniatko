e37e37e3d220add1356c2c9a995cd6488e9a4f39
Yegor-77 <egorhmeluk77@gmail.com>
2026-06-22 19:37:08 +0200

    Y-P-4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import Reviews from "./components/Reviews/Reviews";
-function App() {
-  return (
-    <div>
-      <Reviews/>
 
+  
 import PopularProducts from "./components/PopularProducts/PopularProducts";
 import products from "./data/products.json";
-import { Popular } from "./components/Popular-categories/Popular";
+import { Popular } from "./components/Popular-categories/Popular"
+
 
 function App() {
   return (
     <div>
         <PopularProducts products={products}/>
-      <Popular/>
+      <Popular/> 
+      <Reviews/>
     </div>
   );
  
